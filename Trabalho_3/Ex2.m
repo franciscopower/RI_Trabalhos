440c005dec9244f12f64bd1abe4f54985b9a0e16
Francisco Power <franciscopower66@gmail.com>
2020-11-08 14:12:57 +0000

    adição de ferramenta

--- Trabalho_3/Ex2.m
+++ Trabalho_3/Ex2.m
@@ -25,21 +25,37 @@ rdb = [-0.5 0.5 0]';
 ldb = [-0.5 -0.5 0]';
 ldf = [0.5 -0.5 0]';
 
+% toolT cube points
+ruft = [0.3 0.3 0.6]';
+rubt = [-0.3 0.3 0.6]';
+lubt = [-0.3 -0.3 0.6]';
+luft = [0.3 -0.3 0.6]';
+rdft = [0.3 0.3 0]';
+rdbt = [-0.3 0.3 0]';
+ldbt = [-0.3 -0.3 0]';
+ldft = [0.3 -0.3 0]';
+
 % Create joints
 joint = [luf ldf rdf ruf rub rdb ldb lub;
+        ones(1,8)];
+% Create toolT
+tool = [luft ldft rdft ruft rubt rdbt ldbt lubt;
         ones(1,8)];
 
 % initial transformations
 TB = trans3(0,0,3); 
 TC = trans3(0,0,3); 
+Tt = trans3(0,0,3);
 
 MA1 = eye(4);
 MB1 = TB;
 MC1 = TB*TC;
+Mt1 = TB*TC*Tt;
 
 jointA = joint;
 jointB = MB1*joint; %place arm B in initial position
 jointC = MC1*joint; %place arm C in initial position
+toolT = Mt1*tool; %place toolT in initial position
 
 % Display initial joints
 Af = fill3(jointA(1,1:4), jointA(2,1:4), jointA(3,1:4), 'r');
@@ -57,10 +73,14 @@ Cr = fill3(jointC(1,2:6), jointC(2,2:6), jointC(3,2:6), 'b');
 Cb = fill3(jointC(1,4:8), jointC(2,4:8), jointC(3,4:8), 'b');
 Cl = fill3([jointC(1,1:2) jointC(1,7:8)] , [jointC(2,1:2) jointC(2,7:8)], [jointC(3,1:2) jointC(3,7:8)], 'b');
 
+tf = fill3(toolT(1,1:4), toolT(2,1:4), toolT(3,1:4), 'y');
+tr = fill3(toolT(1,2:6), toolT(2,2:6), toolT(3,2:6), 'y');
+tb = fill3(toolT(1,4:8), toolT(2,4:8), toolT(3,4:8), 'y');
+tl = fill3([toolT(1,1:2) toolT(1,7:8)] , [toolT(2,1:2) toolT(2,7:8)], [toolT(3,1:2) toolT(3,7:8)], 'y');
 
 while true
     
-    % prompt user for rotation angles, in degrees
+    % prompt user for rotation angles, in degrees ------------------------
     prompt = {'Joint 1:','Axis 1:','Joint 2:','Axis 2:','Joint 3:','Axis 3:'};
     dlgtitle = 'Rotation Angles (º)';
     dims = [1 35];
@@ -91,18 +111,20 @@ while true
     aA_incr = linspace(0,aA,frames);
     aB_incr = linspace(0,aB,frames);
     aC_incr = linspace(0,aC,frames);
-
+    % end prompt ---------------------------------------------------------
+    
     %rotate joints
     for i = 1:frames
         % calculate transformation matrices
-        [MA2,MB2,MC2] = rotJoint(1,axisA,aA_incr(i),MA1,MB1,MC1);
-        [MA2,MB2,MC2] = rotJoint(2,axisB,aB_incr(i),MA2,MB2,MC2);
-        [MA2,MB2,MC2] = rotJoint(3,axisC,aC_incr(i),MA2,MB2,MC2);
+        [MA2,MB2,MC2,Mt2] = rotJoint(1,axisA,aA_incr(i),MA1,MB1,MC1,Mt1);
+        [MA2,MB2,MC2,Mt2] = rotJoint(2,axisB,aB_incr(i),MA2,MB2,MC2,Mt2);
+        [MA2,MB2,MC2,Mt2] = rotJoint(3,axisC,aC_incr(i),MA2,MB2,MC2,Mt2);
 
         %calculate new positions of all points
         jointA = MA2*joint;    
         jointB = MB2*joint;
         jointC = MC2*joint;
+        toolT = Mt2*tool;
                
         %display new positions
         set(Af, 'XData',jointA(1,1:4) , 'YData',jointA(2,1:4) , 'ZData',jointA(3,1:4) )
@@ -120,6 +142,11 @@ while true
         set(Cb, 'XData',jointC(1,4:8) , 'YData',jointC(2,4:8) , 'ZData',jointC(3,4:8) )
         set(Cl, 'XData',[jointC(1,1:2) jointC(1,7:8)] , 'YData',[jointC(2,1:2) jointC(2,7:8)] , 'ZData',[jointC(3,1:2) jointC(3,7:8)] )
         
+        set(tf, 'XData',toolT(1,1:4) , 'YData',toolT(2,1:4) , 'ZData',toolT(3,1:4) )
+        set(tr, 'XData',toolT(1,2:6) , 'YData',toolT(2,2:6) , 'ZData',toolT(3,2:6) )
+        set(tb, 'XData',toolT(1,4:8) , 'YData',toolT(2,4:8) , 'ZData',toolT(3,4:8) )
+        set(tl, 'XData',[toolT(1,1:2) toolT(1,7:8)] , 'YData',[toolT(2,1:2) toolT(2,7:8)] , 'ZData',[toolT(3,1:2) toolT(3,7:8)] )
+        
         pause(pause_time)
     end
     
@@ -127,6 +154,7 @@ while true
     MA1 = MA2;
     MB1 = MB2;
     MC1 = MC2;
+    Mt1 = Mt2;
     
 end
 
--- Trabalho_3/rotJoint.m
+++ Trabalho_3/rotJoint.m
@@ -1,4 +1,4 @@
-function [MA2, MB2, MC2] = rotJoint(n_elo, axis, angle, MA1, MB1, MC1)
+function [MA2, MB2, MC2, Mt2] = rotJoint(n_elo, axis, angle, MA1, MB1, MC1, Mt1)
 %ROTELO calculates rotation matrices for a 3 joint robot arm
 %Args:
 %   n_elo: number of joint to rotate (1,2,3)
@@ -13,14 +13,17 @@ if n_elo == 1
     MA2 = rot3(axis,angle)*MA1;
     MB2 = MA2*MA1^-1*MB1;
     MC2 = MB2*MB1^-1*MC1;
+    Mt2 = MC2*MC1^-1*Mt1;
 elseif n_elo == 2
     MA2 = MA1;
     MB2 = MB1*rot3(axis,angle);
     MC2 = MB2*MB1^-1*MC1;
+    Mt2 = MC2*MC1^-1*Mt1;
 elseif n_elo == 3
     MA2 = MA1;
     MB2 = MB1;
     MC2 = MC1*rot3(axis,angle);
+    Mt2 = MC2*MC1^-1*Mt1;
 end
 end
 
