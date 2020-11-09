ade3c126c292a2f56ae24211642eaf463196995b
Francisco Power <franciscopower66@gmail.com>
2020-11-09 10:26:44 +0000

    convert to degrees

--- Trabalho_3/posToAngle.m
+++ Trabalho_3/posToAngle.m
@@ -4,7 +4,7 @@ function [a1z,a1x,a2x,a3x] = posToAngle(pos,Lj,Lt)
 
 [azimuth,elevation,r] = cart2sph(pos(1),pos(2),pos(3));
 
-a1z = azimuth;
+a1z = rad2deg(azimuth);
 t1 = acos((r-Lj)/(2*Lj));
 t2 = acos((cos(t1)*Lj)/(Lj+Lt));
 
