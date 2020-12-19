e3f4d2aa6320f6dc36574b60a410e39d9768c231
Francisco Power <franciscopower66@gmail.com>
2020-12-19 16:04:54 +0000

    elos a mover

--- Trabalho_5/Ex1.m
+++ Trabalho_5/Ex1.m
@@ -7,13 +7,13 @@ figure(1)
 hold on
 axis equal
 grid on
-% axis([-5 15 -15 15 0 15])
+axis([-300 1000 -1000 1000 -250 600])
 xlabel('x')
 ylabel('y')
 zlabel('z')
 view(30,10)
 % Animation settings
-frames = 50;
+frames = 20;
 pause_time = 0.05;
 
 %% Ex1 --------------------------------------------------------------------
@@ -27,10 +27,6 @@ LE = 430;
 LEE = 150;
 LEEE = 280;
 LF = 100;
-%setting animaçao
-frames = 50;
-pause_time = 0.05;
-
 
 theta1 = deg2rad(0);
 theta2 = deg2rad(0);
@@ -73,7 +69,6 @@ r = [p; phi; theta; psi]
 
 % Criar Cilindros dos elos
 
-% basic cylinder
 eloO = createCylinder(50,-250,"xy");
 [cxO, cyO, czO] = transfCylinder(eye(4),eloO);
 eloA = createCylinder(50,-LB,"yz");
@@ -93,31 +88,20 @@ O = surf(cxO, cyO, czO, 'FaceColor', 'r');
 A = surf(cxA, cyA, czA, 'FaceColor', 'r');
 B = surf(cxB, cyB, czB, 'FaceColor', 'g');
 C = surf(cxC, cyC, czC, 'FaceColor', 'b');
-C1 = surf(cxC1, cyC1, czC1, 'FaceColor', 'm');
+C1 = surf(cxC1, cyC1, czC1, 'FaceColor', 'b');
 D = surf(cxD, cyD, czD, 'FaceColor', 'c');
 E = surf(cxE, cyE, czE, 'FaceColor', 'y');
-
-% eloA
-% eloB
-% eloC
-% eloC1
-% eloD
-% eloE
-% tool
-
-
-% Aplicar Transformações iniciais aos cilindros dos elos
 
 % lista de espaços de juntas
 a = {0,0,0,0,0,0;
     35,0,-40,0,50,0;
     35,-40,0,0,50,0;
     105,60,-30,120,-20,40
-    15,30,-30,20,-20,165};
+    15,-30,-30,20,-20,165};
 
 % ciclo de movimentação dos elos
 
-for i=1:5
+for i=2:5
    theta1_incr = linspace(deg2rad(a{i-1,1}),deg2rad(a{i,1}),frames);
    theta2_incr = linspace(deg2rad(a{i-1,2}),deg2rad(a{i,2}),frames);
    theta3_incr = linspace(deg2rad(a{i-1,3}),deg2rad(a{i,3}),frames);
@@ -125,7 +109,7 @@ for i=1:5
    theta5_incr = linspace(deg2rad(a{i-1,5}),deg2rad(a{i,5}),frames);
    theta6_incr = linspace(deg2rad(a{i-1,6}),deg2rad(a{i,6}),frames); 
     
-    for n=1:frame
+    for n=1:frames
         
         %nova angulo para cada angulo ate chegar ao angulo pertendido
         theta1 = theta1_incr(n);
@@ -158,13 +142,13 @@ for i=1:5
         
         
        %[ncx, ncy, ncz] = transfCylinder(OTa,eloO);
-       [ncxA, ncyA, nczA] = transfCylinder(OTa*aTb,eloA);
-       [ncxB, ncyB, nczB] = transfCylinder(OTa*aTb*bTc,eloB);
-       [ncxC, ncyC, nczC] = transfCylinder(OTa*aTb*bTc*cTc1,eloC);
-       [ncxC1, ncyC1, nczC1] = transfCylinder(OTa*aTb*bTc*cTc1*c1Td,eloC1);
-       [ncxD, ncyD, nczD] = transfCylinder(OTa*aTb*bTc*cTc1*c1Td*dTe,eloD);
-       [ncxE, ncyE, nczE] = transfCylinder(OTa*aTb*bTc*cTc1*c1Td*dTe*eTf,eloF);
-       [ncxTool, ncyTool, nczTool] = transfCylinder(OTa*aTb*bTc*cTc1*c1Td*dTe*eTf*fTt,tool);
+       [ncxA, ncyA, nczA] = transfCylinder(OTa,eloA);
+       [ncxB, ncyB, nczB] = transfCylinder(OTa*aTb,eloB);
+       [ncxC, ncyC, nczC] = transfCylinder(OTa*aTb*bTc,eloC);
+       [ncxC1, ncyC1, nczC1] = transfCylinder(OTa*aTb*bTc*cTc1,eloC1);
+       [ncxD, ncyD, nczD] = transfCylinder(OTa*aTb*bTc*cTc1*c1Td,eloD);
+       [ncxE, ncyE, nczE] = transfCylinder(OTa*aTb*bTc*cTc1*c1Td*dTe,eloE);
+%        [ncxTool, ncyTool, nczTool] = transfCylinder(OTa*aTb*bTc*cTc1*c1Td*dTe*eTf*fTt,tool);
         
        set(A, 'XData', ncxA, 'YData', ncyA, 'ZData', nczA)
        set(B, 'XData', ncxB, 'YData', ncyB, 'ZData', nczB) 
@@ -172,9 +156,25 @@ for i=1:5
        set(C1, 'XData', ncxC1, 'YData', ncyC1, 'ZData', nczC1)
        set(D, 'XData', ncxD, 'YData', ncyD, 'ZData', nczD)
        set(E, 'XData', ncxE, 'YData', ncyE, 'ZData', nczE)
-       set(T, 'XData', ncxT, 'YData', ncyT, 'ZData', nczT)
+%        set(T, 'XData', ncxT, 'YData', ncyT, 'ZData', nczT)
+
+       pause(pause_time)
+       
+        BTw = OTa*aTb*bTc*cTc1*c1Td*dTe*eTf*fTt;
+        p = BTw(1:3,4);
+        theta = rad2deg(asin(-BTw(3,1)));
+        phi = rad2deg(atan2(BTw(3,2), BTw(3,3)));
+        psi = rad2deg(atan2(BTw(2,1), BTw(1,1)));
+        % End factor
+        r = [p; phi; theta; psi];
+
+        s1 = "End factor [x y z \phi \theta \psi]: ";
+        s2 = sprintf("[%1.0f %1.0f %1.0f %1.0f %1.0f %1.0f]", r(1), r(2), r(3), r(4), r(5), r(6));
+        s = strcat(s1, s2);
+        title(s)
     end
     
+    pause(0.5)
 end
 
 
