37bd5dca2ef150f9ed9ecc38ab25c82cf76656ec
Francisco Power <franciscopower66@gmail.com>
2020-12-21 18:59:27 +0000

    calculo matriz transf ex2a

---
+++ Trabalho_5/Ex2a.m
@@ -0,0 +1,47 @@
+clear all
+clc
+
+%% Ex1 --------------------------------------------------------------------
+
+% Dados
+syms LA LB LC LD LE LEE LEEE LF
+
+syms theta1 theta2 theta3 theta4 theta5 theta6
+
+syms pwx pwy pwz
+
+% Atribuição do sistema de coordenadas
+%eloN = [theta, alfa, l, d]
+param_eloA = [theta1, -pi/2, LB,LA];
+param_eloB = [theta2-pi/2, pi, LC, 0];
+param_eloC = [theta3, -pi/2, LD, 0];
+param_eloC1 = [0, 0, 0, -LE];
+param_eloD = [theta4, pi/2, 0, 0];
+param_eloE = [theta5, -pi/2, 0, 0];
+param_eloF = [theta6, 0, 0, -LF];
+
+% Transformações de cada elo
+OTa = trans_elo(param_eloA);
+aTb = trans_elo(param_eloB);
+bTc = trans_elo(param_eloC);
+cTc1 = trans_elo(param_eloC1);
+c1Td = trans_elo(param_eloD);
+dTe = trans_elo(param_eloE);
+eTf = trans_elo(param_eloF);
+fTt= rot3("x",pi);
+
+% calculo posicao
+OTw = OTa*aTb*bTc*cTc1;
+OTw = simplify(OTw)
+
+% p = OTw(1:3,4);
+% theta = rad2deg(asin(-OTw(3,1)));
+% phi = rad2deg(atan2(OTw(3,2), OTw(3,3)));
+% psi = rad2deg(atan2(OTw(2,1), OTw(1,1)));
+% 
+% % End factor
+% r = [p; phi; theta; psi];
+
+
+
+
