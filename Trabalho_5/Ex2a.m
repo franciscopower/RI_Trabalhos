ca6934cb87fcc9e56e3de4543e0f503ab01ec983
Francisco Power <franciscopower66@gmail.com>
2020-12-22 18:25:40 +0000

    calculo theta1 e theta3

--- Trabalho_5/Ex2a.m
+++ Trabalho_5/Ex2a.m
@@ -10,6 +10,8 @@ syms theta1 theta2 theta3 theta4 theta5 theta6
 
 syms pwx pwy pwz
 
+syms dp
+
 % Atribuição do sistema de coordenadas
 %eloN = [theta, alfa, l, d]
 param_eloA = [theta1, -pi/2, LB,LA];
@@ -34,7 +36,25 @@ fTt= rot3("x",pi);
 OTw = OTa*aTb*bTc*cTc1;
 OTw = simplify(OTw)
 
-% calculo theta3
+pwx1 = OTw(1,4);
+pwy1 = OTw(2,4);
+pwz1 = OTw(3,4);
+
+% dp = (sqrt(pwx^2 + pwy^2) - LB)^2 + pwz^2
+
+% calculo theta3 (t3)
+d1 = LC*sin(theta2) + LD*sin(theta2+theta3) + LE*cos(theta2+theta3);
+d2 = LC*cos(theta2) + LD*cos(theta2+theta3) - LE*sin(theta2+theta3);
+d = expand(d1^2) + expand(d2^2);
+d = simplify(d)
+
+k1 = -2*LC*LE;
+k2 = 2*LC*LD;
+k3 = dp - LC^2 - LD^2 - LE^2;
+
+t3 = 2 * atan2(k2 + sqrt(k1^2 + k2^2 - k3^2), k1 + k3) %atenção ao +/- da raiz quadrada
+
+% calculo theta2 (t2)
 
 
 
